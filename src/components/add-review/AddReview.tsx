71ee5d845fd34c36a4d8a5aa340b855739cb83cb
Bram Brink <bram@brink.email>
2022-03-23 10:49:17 +0100

    Add review forms

--- src/components/add-review/AddReview.tsx
+++ src/components/add-review/AddReview.tsx
@@ -1,6 +1,6 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import React from "react";
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput, View, TouchableOpacity, StyleSheet } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 interface AddReviewProps {
@@ -8,6 +8,14 @@ interface AddReviewProps {
 }
 
 export const AddReview = ({ navigation }: AddReviewProps) => {
+  type ReviewProps = {
+    name: string;
+    rating: number;
+    comment: string;
+  };
+
+  const [review, setReview] = useState<ReviewProps>({ name: "", rating: 0, comment: "" });
+
   const onClose = () => {
     navigation.goBack();
   };
@@ -18,6 +26,50 @@ export const AddReview = ({ navigation }: AddReviewProps) => {
         <Icon name="close" size={30} />
       </TouchableOpacity>
       <Text style={styles.addReview}>Add Review</Text>
+
+      <TextInput
+        style={styles.name}
+        placeholder="Name (optional)"
+        value={review.name}
+        onChangeText={(name) => {
+          setReview({ ...review, name });
+        }}
+      />
+
+      <View style={styles.stars}>
+        <Text style={styles.rating}>Your rating:</Text>
+
+        <View style={{ flexDirection: "row" }}>
+          {[1, 2, 3, 4, 5].map((rating) => (
+            <TouchableOpacity
+              onPress={() => setReview({ ...review, rating })}
+              style={styles.starButton}
+              key={rating}
+            >
+              <Icon
+                name={"star"}
+                style={{ color: review.rating >= rating ? "#F2C078" : "lightgrey" }}
+                size={24}
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      <TextInput
+        style={styles.comment}
+        placeholder="Review"
+        value={review.comment}
+        multiline={true}
+        numberOfLines={5}
+        onChangeText={(comment) => {
+          setReview({ ...review, comment });
+        }}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Submit review</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -25,7 +77,55 @@ export const AddReview = ({ navigation }: AddReviewProps) => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    padding: 20,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
   },
-  addReview: {},
+  addReview: {
+    fontSize: 28,
+    textAlign: "center",
+    paddingTop: 20,
+    fontWeight: "600",
+  },
+  name: {
+    height: 40,
+    width: "auto",
+    backgroundColor: "#FAEDCA",
+    borderRadius: 4,
+    padding: 10,
+    marginTop: 20,
+  },
+  rating: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  stars: {
+    flexDirection: "row",
+    marginTop: 10,
+    alignItems: "baseline",
+    justifyContent: "space-between",
+  },
+  starButton: {
+    padding: 5,
+  },
+  comment: {
+    backgroundColor: "#FAEDCA",
+    borderRadius: 4,
+    height: 200,
+    maxHeight: 200,
+    marginTop: 20,
+    padding: 10,
+    width: "auto",
+  },
+  button: {
+    backgroundColor: "#7EBC89",
+    borderRadius: 3,
+    marginTop: 40,
+    padding: 10,
+    width: "auto",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "700",
+  },
 });
